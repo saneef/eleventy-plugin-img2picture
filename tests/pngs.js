e68f81f8a2b7624a319e5a0e6afa6587773d8bf6
saneef <hello@saneef.com>
2021-07-19 09:11:23 +0530

    test: use non-hashed filenames on tests
    Different hashes were produced on Github CI.

--- tests/pngs.js
+++ tests/pngs.js
@@ -5,6 +5,7 @@ const rimraf = promisify(require("rimraf"));
 const imageSize = require("image-size");
 
 const img2picture = require("../lib/img2picture.js");
+const { filenameFormat } = require("./utils.js");
 
 const sourcePath = path.join(__dirname, "fixtures");
 const outputBase = path.join(__dirname, "output", "pngs");
@@ -16,21 +17,22 @@ test("Optimizes PNG image, puts them in <picture>, and generates AVIF, WebP, JPE
   const input = '<img src="/images/shapes.png" alt="Shapes">';
   const outputPath = "file.html";
   const output =
-    '<picture><source type="image/avif" srcset="/images/shapes-f4474c14-150w.avif 150w, /images/shapes-f4474c14-300w.avif 300w, /images/shapes-f4474c14-450w.avif 450w, /images/shapes-f4474c14-600w.avif 600w, /images/shapes-f4474c14-750w.avif 750w, /images/shapes-f4474c14-900w.avif 900w, /images/shapes-f4474c14-1050w.avif 1050w, /images/shapes-f4474c14-1200w.avif 1200w, /images/shapes-f4474c14-1350w.avif 1350w" sizes="100vw"><source type="image/webp" srcset="/images/shapes-f4474c14-150w.webp 150w, /images/shapes-f4474c14-300w.webp 300w, /images/shapes-f4474c14-450w.webp 450w, /images/shapes-f4474c14-600w.webp 600w, /images/shapes-f4474c14-750w.webp 750w, /images/shapes-f4474c14-900w.webp 900w, /images/shapes-f4474c14-1050w.webp 1050w, /images/shapes-f4474c14-1200w.webp 1200w, /images/shapes-f4474c14-1350w.webp 1350w" sizes="100vw"><source type="image/jpeg" srcset="/images/shapes-f4474c14-150w.jpeg 150w, /images/shapes-f4474c14-300w.jpeg 300w, /images/shapes-f4474c14-450w.jpeg 450w, /images/shapes-f4474c14-600w.jpeg 600w, /images/shapes-f4474c14-750w.jpeg 750w, /images/shapes-f4474c14-900w.jpeg 900w, /images/shapes-f4474c14-1050w.jpeg 1050w, /images/shapes-f4474c14-1200w.jpeg 1200w, /images/shapes-f4474c14-1350w.jpeg 1350w" sizes="100vw"><img src="/images/shapes-f4474c14-150w.jpeg" width="1350" height="1350" alt="Shapes" sizes="100vw" loading="lazy" decoding="async"></picture>';
+    '<picture><source type="image/avif" srcset="/images/shapes-150w.avif 150w, /images/shapes-300w.avif 300w, /images/shapes-450w.avif 450w, /images/shapes-600w.avif 600w, /images/shapes-750w.avif 750w, /images/shapes-900w.avif 900w, /images/shapes-1050w.avif 1050w, /images/shapes-1200w.avif 1200w, /images/shapes-1350w.avif 1350w" sizes="100vw"><source type="image/webp" srcset="/images/shapes-150w.webp 150w, /images/shapes-300w.webp 300w, /images/shapes-450w.webp 450w, /images/shapes-600w.webp 600w, /images/shapes-750w.webp 750w, /images/shapes-900w.webp 900w, /images/shapes-1050w.webp 1050w, /images/shapes-1200w.webp 1200w, /images/shapes-1350w.webp 1350w" sizes="100vw"><source type="image/jpeg" srcset="/images/shapes-150w.jpeg 150w, /images/shapes-300w.jpeg 300w, /images/shapes-450w.jpeg 450w, /images/shapes-600w.jpeg 600w, /images/shapes-750w.jpeg 750w, /images/shapes-900w.jpeg 900w, /images/shapes-1050w.jpeg 1050w, /images/shapes-1200w.jpeg 1200w, /images/shapes-1350w.jpeg 1350w" sizes="100vw"><img src="/images/shapes-150w.jpeg" width="1350" height="1350" alt="Shapes" sizes="100vw" loading="lazy" decoding="async"></picture>';
 
   const transformer = img2picture({
     input: sourcePath,
     output: outputBase,
     urlPath: "/images/",
+    filenameFormat,
   });
   const result = await transformer(input, outputPath);
   t.is(result, output);
 
   const { width: smallestWebPWidth } = imageSize(
-    path.join(outputBase, "shapes-f4474c14-150w.webp")
+    path.join(outputBase, "shapes-150w.webp")
   );
   const { width: largestWebPWidth } = imageSize(
-    path.join(outputBase, "shapes-f4474c14-1350w.webp")
+    path.join(outputBase, "shapes-1350w.webp")
   );
 
   t.is(smallestWebPWidth, 150);
