47063d1044b117504d32b45e62c960b756740171
Saneef Ansari <hello@saneef.com>
2023-12-02 10:54:09 +0530

    Uses `generateObject` from Eleventy Image for `<picture>` HTML generation
    - **Breaking**: The fallback `<img>` will have the smallest width file instead of medium width.
    - These changes were made in order to add support for SVG in future

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,13 @@
 const img2picture = require("./lib/img2picture.js");
 
-/** @typedef { import('./img2picture').Img2PictureOptions } Img2PictureOptions */
+/** @typedef {import("./lib/img2picture").Img2PictureOptions} Img2PictureOptions */
 
 module.exports = {
   /**
    * Plugin config function
    *
-   * @param      {object}  eleventy     The eleventy configuration object
-   * @param      {Img2PictureOptions=}       The options
+   * @param {object} eleventy The eleventy configuration object
+   * @param {Img2PictureOptions} [The] Options
    */
   configFunction(eleventy, options) {
     eleventy.addTransform("img2picture", img2picture(options));
--- tests/attributes.js
+++ tests/attributes.js
@@ -18,95 +18,79 @@ const baseConfig = {
 test('Retain alt=""', async (t) => {
   const input = '<img src="/images/shapes.png" alt="">';
   const outputPath = "file.html";
-  const output =
-    '<html><head></head><body><picture><source type="image/avif" srcset="/images/shapes-150w.avif 150w, /images/shapes-300w.avif 300w, /images/shapes-450w.avif 450w, /images/shapes-600w.avif 600w, /images/shapes-750w.avif 750w, /images/shapes-900w.avif 900w, /images/shapes-1050w.avif 1050w, /images/shapes-1200w.avif 1200w, /images/shapes-1350w.avif 1350w" sizes="100vw"><source type="image/webp" srcset="/images/shapes-150w.webp 150w, /images/shapes-300w.webp 300w, /images/shapes-450w.webp 450w, /images/shapes-600w.webp 600w, /images/shapes-750w.webp 750w, /images/shapes-900w.webp 900w, /images/shapes-1050w.webp 1050w, /images/shapes-1200w.webp 1200w, /images/shapes-1350w.webp 1350w" sizes="100vw"><source type="image/jpeg" srcset="/images/shapes-150w.jpeg 150w, /images/shapes-300w.jpeg 300w, /images/shapes-450w.jpeg 450w, /images/shapes-600w.jpeg 600w, /images/shapes-750w.jpeg 750w, /images/shapes-900w.jpeg 900w, /images/shapes-1050w.jpeg 1050w, /images/shapes-1200w.jpeg 1200w, /images/shapes-1350w.jpeg 1350w" sizes="100vw"><img src="/images/shapes-750w.jpeg" width="1350" height="1350" alt="" loading="lazy" decoding="async"></picture></body></html>';
 
   const transformer = img2picture(baseConfig);
   const result = await transformer(input, outputPath);
-  t.is(result, output);
+  t.snapshot(result);
 });
 
 test("Don't hoist 'class' from <img> on <picture> when 'hoistImgClass=false", async (t) => {
   const input = '<img class="w-full" src="/images/shapes.png" alt="Shapes">';
   const outputPath = "file.html";
-  const output =
-    '<html><head></head><body><picture><source type="image/avif" srcset="/images/shapes-150w.avif 150w, /images/shapes-300w.avif 300w, /images/shapes-450w.avif 450w, /images/shapes-600w.avif 600w, /images/shapes-750w.avif 750w, /images/shapes-900w.avif 900w, /images/shapes-1050w.avif 1050w, /images/shapes-1200w.avif 1200w, /images/shapes-1350w.avif 1350w" sizes="100vw"><source type="image/webp" srcset="/images/shapes-150w.webp 150w, /images/shapes-300w.webp 300w, /images/shapes-450w.webp 450w, /images/shapes-600w.webp 600w, /images/shapes-750w.webp 750w, /images/shapes-900w.webp 900w, /images/shapes-1050w.webp 1050w, /images/shapes-1200w.webp 1200w, /images/shapes-1350w.webp 1350w" sizes="100vw"><source type="image/jpeg" srcset="/images/shapes-150w.jpeg 150w, /images/shapes-300w.jpeg 300w, /images/shapes-450w.jpeg 450w, /images/shapes-600w.jpeg 600w, /images/shapes-750w.jpeg 750w, /images/shapes-900w.jpeg 900w, /images/shapes-1050w.jpeg 1050w, /images/shapes-1200w.jpeg 1200w, /images/shapes-1350w.jpeg 1350w" sizes="100vw"><img src="/images/shapes-750w.jpeg" width="1350" height="1350" class="w-full" alt="Shapes" loading="lazy" decoding="async"></picture></body></html>';
 
   const transformer = img2picture(baseConfig);
   const result = await transformer(input, outputPath);
-  t.is(result, output);
+  t.snapshot(result);
 });
 
 test("Hoist 'class' from <img> on <picture> when 'hoistImgClass=true", async (t) => {
   const config = { ...baseConfig, hoistImgClass: true };
   const input = '<img class="w-full" src="/images/shapes.png" alt="Shapes">';
   const outputPath = "file.html";
-  const output =
-    '<html><head></head><body><picture class="w-full"><source type="image/avif" srcset="/images/shapes-150w.avif 150w, /images/shapes-300w.avif 300w, /images/shapes-450w.avif 450w, /images/shapes-600w.avif 600w, /images/shapes-750w.avif 750w, /images/shapes-900w.avif 900w, /images/shapes-1050w.avif 1050w, /images/shapes-1200w.avif 1200w, /images/shapes-1350w.avif 1350w" sizes="100vw"><source type="image/webp" srcset="/images/shapes-150w.webp 150w, /images/shapes-300w.webp 300w, /images/shapes-450w.webp 450w, /images/shapes-600w.webp 600w, /images/shapes-750w.webp 750w, /images/shapes-900w.webp 900w, /images/shapes-1050w.webp 1050w, /images/shapes-1200w.webp 1200w, /images/shapes-1350w.webp 1350w" sizes="100vw"><source type="image/jpeg" srcset="/images/shapes-150w.jpeg 150w, /images/shapes-300w.jpeg 300w, /images/shapes-450w.jpeg 450w, /images/shapes-600w.jpeg 600w, /images/shapes-750w.jpeg 750w, /images/shapes-900w.jpeg 900w, /images/shapes-1050w.jpeg 1050w, /images/shapes-1200w.jpeg 1200w, /images/shapes-1350w.jpeg 1350w" sizes="100vw"><img src="/images/shapes-750w.jpeg" width="1350" height="1350" alt="Shapes" loading="lazy" decoding="async"></picture></body></html>';
 
   const transformer = img2picture(config);
   const result = await transformer(input, outputPath);
-  t.is(result, output);
+  t.snapshot(result);
 });
 
 test("Hoist 'sizes' attribute from <img> on <source>", async (t) => {
   const input =
     '<img src="/images/shapes.png" sizes="(min-width: 60em) 75vw, 100vw" alt="Shapes">';
   const outputPath = "file.html";
-  const output =
-    '<html><head></head><body><picture><source type="image/avif" srcset="/images/shapes-150w.avif 150w, /images/shapes-300w.avif 300w, /images/shapes-450w.avif 450w, /images/shapes-600w.avif 600w, /images/shapes-750w.avif 750w, /images/shapes-900w.avif 900w, /images/shapes-1050w.avif 1050w, /images/shapes-1200w.avif 1200w, /images/shapes-1350w.avif 1350w" sizes="(min-width: 60em) 75vw, 100vw"><source type="image/webp" srcset="/images/shapes-150w.webp 150w, /images/shapes-300w.webp 300w, /images/shapes-450w.webp 450w, /images/shapes-600w.webp 600w, /images/shapes-750w.webp 750w, /images/shapes-900w.webp 900w, /images/shapes-1050w.webp 1050w, /images/shapes-1200w.webp 1200w, /images/shapes-1350w.webp 1350w" sizes="(min-width: 60em) 75vw, 100vw"><source type="image/jpeg" srcset="/images/shapes-150w.jpeg 150w, /images/shapes-300w.jpeg 300w, /images/shapes-450w.jpeg 450w, /images/shapes-600w.jpeg 600w, /images/shapes-750w.jpeg 750w, /images/shapes-900w.jpeg 900w, /images/shapes-1050w.jpeg 1050w, /images/shapes-1200w.jpeg 1200w, /images/shapes-1350w.jpeg 1350w" sizes="(min-width: 60em) 75vw, 100vw"><img src="/images/shapes-750w.jpeg" width="1350" height="1350" alt="Shapes" loading="lazy" decoding="async"></picture></body></html>';
 
   const transformer = img2picture(baseConfig);
   const result = await transformer(input, outputPath);
-  t.is(result, output);
+  t.snapshot(result);
 });
 
 test("Retain loading and decoding attributes on <img>", async (t) => {
   const input =
     '<img src="/images/shapes.png" loading="eager" decoding="auto" alt="Shapes">';
   const outputPath = "file.html";
-  const output =
-    '<html><head></head><body><picture><source type="image/avif" srcset="/images/shapes-150w.avif 150w, /images/shapes-300w.avif 300w, /images/shapes-450w.avif 450w, /images/shapes-600w.avif 600w, /images/shapes-750w.avif 750w, /images/shapes-900w.avif 900w, /images/shapes-1050w.avif 1050w, /images/shapes-1200w.avif 1200w, /images/shapes-1350w.avif 1350w" sizes="100vw"><source type="image/webp" srcset="/images/shapes-150w.webp 150w, /images/shapes-300w.webp 300w, /images/shapes-450w.webp 450w, /images/shapes-600w.webp 600w, /images/shapes-750w.webp 750w, /images/shapes-900w.webp 900w, /images/shapes-1050w.webp 1050w, /images/shapes-1200w.webp 1200w, /images/shapes-1350w.webp 1350w" sizes="100vw"><source type="image/jpeg" srcset="/images/shapes-150w.jpeg 150w, /images/shapes-300w.jpeg 300w, /images/shapes-450w.jpeg 450w, /images/shapes-600w.jpeg 600w, /images/shapes-750w.jpeg 750w, /images/shapes-900w.jpeg 900w, /images/shapes-1050w.jpeg 1050w, /images/shapes-1200w.jpeg 1200w, /images/shapes-1350w.jpeg 1350w" sizes="100vw"><img src="/images/shapes-750w.jpeg" width="1350" height="1350" loading="eager" decoding="auto" alt="Shapes"></picture></body></html>';
 
   const transformer = img2picture(baseConfig);
   const result = await transformer(input, outputPath);
-  t.is(result, output);
+  t.snapshot(result);
 });
 
 test("Retain all data attributes on <img>", async (t) => {
   const input =
     '<img src="/images/shapes.png" alt="Shapes" data-size="full" aria-label="An icon">';
   const outputPath = "file.html";
-  const output =
-    '<html><head></head><body><picture><source type="image/avif" srcset="/images/shapes-150w.avif 150w, /images/shapes-300w.avif 300w, /images/shapes-450w.avif 450w, /images/shapes-600w.avif 600w, /images/shapes-750w.avif 750w, /images/shapes-900w.avif 900w, /images/shapes-1050w.avif 1050w, /images/shapes-1200w.avif 1200w, /images/shapes-1350w.avif 1350w" sizes="100vw"><source type="image/webp" srcset="/images/shapes-150w.webp 150w, /images/shapes-300w.webp 300w, /images/shapes-450w.webp 450w, /images/shapes-600w.webp 600w, /images/shapes-750w.webp 750w, /images/shapes-900w.webp 900w, /images/shapes-1050w.webp 1050w, /images/shapes-1200w.webp 1200w, /images/shapes-1350w.webp 1350w" sizes="100vw"><source type="image/jpeg" srcset="/images/shapes-150w.jpeg 150w, /images/shapes-300w.jpeg 300w, /images/shapes-450w.jpeg 450w, /images/shapes-600w.jpeg 600w, /images/shapes-750w.jpeg 750w, /images/shapes-900w.jpeg 900w, /images/shapes-1050w.jpeg 1050w, /images/shapes-1200w.jpeg 1200w, /images/shapes-1350w.jpeg 1350w" sizes="100vw"><img src="/images/shapes-750w.jpeg" width="1350" height="1350" alt="Shapes" data-size="full" aria-label="An icon" loading="lazy" decoding="async"></picture></body></html>';
 
   const transformer = img2picture(baseConfig);
   const result = await transformer(input, outputPath);
-  t.is(result, output);
+  t.snapshot(result);
 });
 
 test("Generate image sizes based on data-img2picture-widths", async (t) => {
   const input =
     '<img src="/images/shapes.png" alt="Shapes" data-img2picture-widths="100,150,200,250,300">';
   const outputPath = "file.html";
-  const output =
-    '<html><head></head><body><picture><source type="image/avif" srcset="/images/shapes-100w.avif 100w, /images/shapes-150w.avif 150w, /images/shapes-200w.avif 200w, /images/shapes-250w.avif 250w, /images/shapes-300w.avif 300w" sizes="100vw"><source type="image/webp" srcset="/images/shapes-100w.webp 100w, /images/shapes-150w.webp 150w, /images/shapes-200w.webp 200w, /images/shapes-250w.webp 250w, /images/shapes-300w.webp 300w" sizes="100vw"><source type="image/jpeg" srcset="/images/shapes-100w.jpeg 100w, /images/shapes-150w.jpeg 150w, /images/shapes-200w.jpeg 200w, /images/shapes-250w.jpeg 250w, /images/shapes-300w.jpeg 300w" sizes="100vw"><img src="/images/shapes-200w.jpeg" width="300" height="300" alt="Shapes" loading="lazy" decoding="async"></picture></body></html>';
 
   const transformer = img2picture(baseConfig);
   const result = await transformer(input, outputPath);
-  t.is(result, output);
+  t.snapshot(result);
 });
 
 test("Populate <picture> class from data-img2picture-picture-class", async (t) => {
   const input =
     '<img src="/images/shapes.png" alt="Shapes" data-img2picture-picture-class="w-full">';
   const outputPath = "file.html";
-  const output =
-    '<html><head></head><body><picture class="w-full"><source type="image/avif" srcset="/images/shapes-150w.avif 150w, /images/shapes-300w.avif 300w, /images/shapes-450w.avif 450w, /images/shapes-600w.avif 600w, /images/shapes-750w.avif 750w, /images/shapes-900w.avif 900w, /images/shapes-1050w.avif 1050w, /images/shapes-1200w.avif 1200w, /images/shapes-1350w.avif 1350w" sizes="100vw"><source type="image/webp" srcset="/images/shapes-150w.webp 150w, /images/shapes-300w.webp 300w, /images/shapes-450w.webp 450w, /images/shapes-600w.webp 600w, /images/shapes-750w.webp 750w, /images/shapes-900w.webp 900w, /images/shapes-1050w.webp 1050w, /images/shapes-1200w.webp 1200w, /images/shapes-1350w.webp 1350w" sizes="100vw"><source type="image/jpeg" srcset="/images/shapes-150w.jpeg 150w, /images/shapes-300w.jpeg 300w, /images/shapes-450w.jpeg 450w, /images/shapes-600w.jpeg 600w, /images/shapes-750w.jpeg 750w, /images/shapes-900w.jpeg 900w, /images/shapes-1050w.jpeg 1050w, /images/shapes-1200w.jpeg 1200w, /images/shapes-1350w.jpeg 1350w" sizes="100vw"><img src="/images/shapes-750w.jpeg" width="1350" height="1350" alt="Shapes" loading="lazy" decoding="async"></picture></body></html>';
 
   const transformer = img2picture(baseConfig);
   const result = await transformer(input, outputPath);
-  t.is(result, output);
+  t.snapshot(result);
 });
 
 test("Populate <picture> class from data-img2picture-picture-class when 'hoistImgClass=true", async (t) => {
@@ -114,10 +98,8 @@ test("Populate <picture> class from data-img2picture-picture-class when 'hoistIm
   const input =
     '<img class="img" src="/images/shapes.png" alt="Shapes" data-img2picture-picture-class="w-full">';
   const outputPath = "file.html";
-  const output =
-    '<html><head></head><body><picture class="w-full img"><source type="image/avif" srcset="/images/shapes-150w.avif 150w, /images/shapes-300w.avif 300w, /images/shapes-450w.avif 450w, /images/shapes-600w.avif 600w, /images/shapes-750w.avif 750w, /images/shapes-900w.avif 900w, /images/shapes-1050w.avif 1050w, /images/shapes-1200w.avif 1200w, /images/shapes-1350w.avif 1350w" sizes="100vw"><source type="image/webp" srcset="/images/shapes-150w.webp 150w, /images/shapes-300w.webp 300w, /images/shapes-450w.webp 450w, /images/shapes-600w.webp 600w, /images/shapes-750w.webp 750w, /images/shapes-900w.webp 900w, /images/shapes-1050w.webp 1050w, /images/shapes-1200w.webp 1200w, /images/shapes-1350w.webp 1350w" sizes="100vw"><source type="image/jpeg" srcset="/images/shapes-150w.jpeg 150w, /images/shapes-300w.jpeg 300w, /images/shapes-450w.jpeg 450w, /images/shapes-600w.jpeg 600w, /images/shapes-750w.jpeg 750w, /images/shapes-900w.jpeg 900w, /images/shapes-1050w.jpeg 1050w, /images/shapes-1200w.jpeg 1200w, /images/shapes-1350w.jpeg 1350w" sizes="100vw"><img src="/images/shapes-750w.jpeg" width="1350" height="1350" alt="Shapes" loading="lazy" decoding="async"></picture></body></html>';
 
   const transformer = img2picture(config);
   const result = await transformer(input, outputPath);
-  t.is(result, output);
+  t.snapshot(result);
 });
